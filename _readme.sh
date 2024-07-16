aac3402a7be538b45d664eed776a1b5351c8c616
boobie <boobie@riseup.net>
2024-07-15 23:51:29 -0400

    2627d03cc90e1a8a941789d5f9205f6c021ddee415f47a511279dab285c5f961  -

---
+++ _readme.sh
@@ -0,0 +1,28 @@
+echo "
+# markdown file encryption and obfuscation
+this project encrypts all markdown (.md) files in the current directory and obfuscates their filenames for added privacy.
+
+oh, it excludes readme.md too, so you have to run this.
+
+## features
+- encrypts all .md files in the current directory
+- obfuscates filenames using md5 hash
+- uses strong gpg encryption (aes256, sha512)
+- creates a mapping file to track original and obfuscated filenames
+- encrypts the mapping file for security
+
+## usage: view readme
+yarn readme
+
+## usage: encrypt all .md files
+yarn encrypt
+
+## usage: decrypt all gpg files
+yarn decrypt
+
+## usage: generate a totp code
+yarn totp 'totpcodehere'
+
+## usage: run test suite
+yarn test
+"
